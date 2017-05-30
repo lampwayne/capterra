719149548acd267c440baf1ad3b09ac7bb6f01a4
stivenjoe <2925528279@qq.com>
2017-05-30 11:38:53 +0800

    修改配置前缀

--- Application/User/Conf/config.php
+++ Application/User/Conf/config.php
@@ -16,4 +16,4 @@ define('UC_APP_ID', 1); //应用ID
 define('UC_API_TYPE', 'Model'); //可选值 Model / Service
 define('UC_AUTH_KEY', '=(YBnV`*|p!ZF,w6fCqs>iO}zbHe[@_Ko+J)#NE?'); //加密KEY
 define('UC_DB_DSN', 'mysqli://root:root@127.0.0.1:3306/capterra'); // 数据库连接，使用Model方式调用API必须配置此项
-define('UC_TABLE_PREFIX', 'onethink_'); // 数据表前缀，使用Model方式调用API必须配置此项
+define('UC_TABLE_PREFIX', 'capterra_'); // 数据表前缀，使用Model方式调用API必须配置此项
